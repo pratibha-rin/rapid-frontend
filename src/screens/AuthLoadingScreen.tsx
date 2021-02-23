7902d8c0b3232e5c4da78f898f913cfdc6ece205
pratibha-rin <59640782+pratibha-rin@users.noreply.github.com>
2021-02-23 21:03:30 +0700

    Add files via upload

---
+++ src/screens/AuthLoadingScreen.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+    ActivityIndicator,
+    StatusBar,
+    View,
+} from 'react-native';
+import firebase from 'firebase'
+
+class AuthLoadingScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        try {
+            firebase.auth().onAuthStateChanged((user) => {
+                if (user) {
+                    console.log(user.email)
+                    props.navigation.navigate('Dashboard')
+                } else {
+                    props.navigation.navigate('HomeScreen')
+                }
+            })
+        } catch (e) {
+            props.navigation.navigate('HomeScreen')
+        }
+    }
+
+    render() {
+        return (
+            <View>
+                <ActivityIndicator />
+                <StatusBar barStyle="default" />
+            </View>
+        );
+    }
+}
+
+export default AuthLoadingScreen;
